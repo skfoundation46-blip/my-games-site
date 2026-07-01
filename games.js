aa5a21800a1718cedfad7b6fc99ddae7c295898a
skfoundation46-blip <skfoundation46@gmail.com>
2026-07-01 22:18:41 +0530

    Update games.js

--- games.js
+++ games.js
@@ -1,1 +1,141 @@
-
+const games = [
+{
+name:"101 Z",
+bonus:"₹800 Bonus",
+redeem:"Min ₹100",
+image:"images/101z.png",
+link:"#"
+},
+{
+name:"MY 777",
+bonus:"₹1200 Bonus",
+redeem:"Min ₹100",
+image:"images/my777.png",
+link:"#"
+},
+{
+name:"51 Game",
+bonus:"₹28 Bonus",
+redeem:"Min ₹100",
+image:"images/51game.png",
+link:"#"
+},
+{
+name:"91 Club",
+bonus:"₹100 Bonus",
+redeem:"Min ₹100",
+image:"images/91club.png",
+link:"#"
+},
+{
+name:"BDG Win",
+bonus:"₹55 Bonus",
+redeem:"Min ₹100",
+image:"images/bdgwin.png",
+link:"#"
+},
+{
+name:"Daman Game",
+bonus:"₹50 Bonus",
+redeem:"Min ₹100",
+image:"images/daman.png",
+link:"#"
+},
+{
+name:"Tiranga",
+bonus:"₹50 Bonus",
+redeem:"Min ₹100",
+image:"images/tiranga.png",
+link:"#"
+},
+{
+name:"Goa Games",
+bonus:"₹50 Bonus",
+redeem:"Min ₹100",
+image:"images/goa.png",
+link:"#"
+},
+{
+name:"KWG Game",
+bonus:"₹66 Bonus",
+redeem:"Min ₹100",
+image:"images/kwg.png",
+link:"#"
+},
+{
+name:"Raja Luck",
+bonus:"₹50 Bonus",
+redeem:"Min ₹100",
+image:"images/rajaluck.png",
+link:"#"
+},
+{
+name:"Big Mumbai",
+bonus:"₹100 Bonus",
+redeem:"Min ₹100",
+image:"images/bigmumbai.png",
+link:"#"
+},
+{
+name:"WinGo",
+bonus:"₹50 Bonus",
+redeem:"Min ₹100",
+image:"images/wingo.png",
+link:"#"
+},
+{
+name:"Lucky Spin",
+bonus:"₹88 Bonus",
+redeem:"Min ₹100",
+image:"images/luckyspin.png",
+link:"#"
+},
+{
+name:"Royal Win",
+bonus:"₹100 Bonus",
+redeem:"Min ₹100",
+image:"images/royalwin.png",
+link:"#"
+},
+{
+name:"Jackpot Pro",
+bonus:"₹120 Bonus",
+redeem:"Min ₹100",
+image:"images/jackpotpro.png",
+link:"#"
+},
+{
+name:"Casino 777",
+bonus:"₹150 Bonus",
+redeem:"Min ₹100",
+image:"images/casino777.png",
+link:"#"
+},
+{
+name:"Rummy 365",
+bonus:"₹1500 Bonus",
+redeem:"Min ₹100",
+image:"images/rummy365.png",
+link:"#"
+},
+{
+name:"A23 Rummy",
+bonus:"₹151 Bonus",
+redeem:"Min ₹100",
+image:"images/a23.png",
+link:"#"
+},
+{
+name:"Junglee Rummy",
+bonus:"₹51 Bonus",
+redeem:"Min ₹100",
+image:"images/junglee.png",
+link:"#"
+},
+{
+name:"Teen Patti Master",
+bonus:"₹500 Bonus",
+redeem:"Min ₹100",
+image:"images/tpm.png",
+link:"#"
+},
