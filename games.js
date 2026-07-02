4f5a762a62650a1b21c4b6792a356c98fecca8ed
skfoundation46-blip <skfoundation46@gmail.com>
2026-07-02 16:30:07 +0530

    Update games.js

--- games.js
+++ games.js
@@ -1,354 +1,45 @@
 const games = [
+
 {
-name:"101 Z",
-bonus:"₹800 Bonus",
-redeem:"Min ₹100",
-image:"images/101z.png",
-link:"#"
+  name:"ABC Rummy",
+  bonus:"₹100 Bonus",
+  redeem:"Min ₹100",
+  image:"images/abcrummy.png",
+  link:"https://www.11abcrummy.com/?code=75CAZX4PBFD&t=1782980303"
 },
+
 {
-name:"MY 777",
-bonus:"₹1200 Bonus",
-redeem:"Min ₹100",
-image:"images/my777.png",
-link:"#"
+  name:"Bet213",
+  bonus:"₹100 Bonus",
+  redeem:"Min ₹100",
+  image:"images/bet213.png",
+  link:"https://www.bet213.pro/?code=2QTYVYTR77F&t=1782988092"
 },
+
 {
-name:"51 Game",
-bonus:"₹28 Bonus",
-redeem:"Min ₹100",
-image:"images/51game.png",
-link:"#"
+  name:"Bingo 101",
+  bonus:"₹900 Bonus",
+  redeem:"Min ₹100",
+  image:"images/bingo101.png",
+  link:"https://bingo101.info/?code=H73SL4DYWJW&t=1782988177"
 },
+
 {
-name:"91 Club",
-bonus:"₹100 Bonus",
-redeem:"Min ₹100",
-image:"images/91club.png",
-link:"#"
+  name:"Boss Rummy",
+  bonus:"₹100 Bonus",
+  redeem:"Min ₹100",
+  image:"images/bossrummy.png",
+  link:"https://www.bossrummyo.com/?code=9HFAK694TXD&t=1782988271"
 },
+
 {
-name:"BDG Win",
-bonus:"₹55 Bonus",
-redeem:"Min ₹100",
-image:"images/bdgwin.png",
-link:"#"
-},
-{
-name:"Daman Game",
-bonus:"₹50 Bonus",
-redeem:"Min ₹100",
-image:"images/daman.png",
-link:"#"
-},
-{
-name:"Tiranga",
-bonus:"₹50 Bonus",
-redeem:"Min ₹100",
-image:"images/tiranga.png",
-link:"#"
-},
-{
-name:"Goa Games",
-bonus:"₹50 Bonus",
-redeem:"Min ₹100",
-image:"images/goa.png",
-link:"#"
-},
-{
-name:"KWG Game",
-bonus:"₹66 Bonus",
-redeem:"Min ₹100",
-image:"images/kwg.png",
-link:"#"
-},
-{
-name:"Raja Luck",
-bonus:"₹50 Bonus",
-redeem:"Min ₹100",
-image:"images/rajaluck.png",
-link:"#"
-},
-{
-name:"Big Mumbai",
-bonus:"₹100 Bonus",
-redeem:"Min ₹100",
-image:"images/bigmumbai.png",
-link:"#"
-},
-{
-name:"WinGo",
-bonus:"₹50 Bonus",
-redeem:"Min ₹100",
-image:"images/wingo.png",
-link:"#"
-},
-{
-name:"Lucky Spin",
-bonus:"₹88 Bonus",
-redeem:"Min ₹100",
-image:"images/luckyspin.png",
-link:"#"
-},
-{
-name:"Royal Win",
-bonus:"₹100 Bonus",
-redeem:"Min ₹100",
-image:"images/royalwin.png",
-link:"#"
-},
-{
-name:"Jackpot Pro",
-bonus:"₹120 Bonus",
-redeem:"Min ₹100",
-image:"images/jackpotpro.png",
-link:"#"
-},
-{
-name:"Casino 777",
-bonus:"₹150 Bonus",
-redeem:"Min ₹100",
-image:"images/casino777.png",
-link:"#"
-},
-{
-name:"Rummy 365",
-bonus:"₹1500 Bonus",
-redeem:"Min ₹100",
-image:"images/rummy365.png",
-link:"#"
-},
-{
-name:"A23 Rummy",
-bonus:"₹151 Bonus",
-redeem:"Min ₹100",
-image:"images/a23.png",
-link:"#"
-},
-{
-name:"Junglee Rummy",
-bonus:"₹51 Bonus",
-redeem:"Min ₹100",
-image:"images/junglee.png",
-link:"#"
-},
-{
-name:"Teen Patti Master",
-bonus:"₹500 Bonus",
-redeem:"Min ₹100",
-image:"images/tpm.png",
-link:"#"
-},
-{
-name:"Teen Patti Gold",
-bonus:"₹100 Bonus",
-redeem:"Min ₹100",
-image:"images/tpgold.png",
-link:"#"
-},
-{
-name:"Teen Patti Lucky",
-bonus:"₹100 Bonus",
-redeem:"Min ₹100",
-image:"images/tpluck.png",
-link:"#"
-},
-{
-name:"Teen Patti Star",
-bonus:"₹100 Bonus",
-redeem:"Min ₹100",
-image:"images/tpstar.png",
-link:"#"
-},
-{
-name:"Teen Patti Joy",
-bonus:"₹100 Bonus",
-redeem:"Min ₹100",
-image:"images/tpjoy.png",
-link:"#"
-},
-{
-name:"Teen Patti Ace",
-bonus:"₹100 Bonus",
-redeem:"Min ₹100",
-image:"images/tpace.png",
-link:"#"
-},
-{
-name:"Teen Patti Cash",
-bonus:"₹100 Bonus",
-redeem:"Min ₹100",
-image:"images/tpcash.png",
-link:"#"
-},
-{
-name:"Yono VIP",
-bonus:"₹500 Bonus",
-redeem:"Min ₹100",
-image:"images/yonovip.png",
-link:"#"
-},
-{
-name:"Yono Rummy",
-bonus:"₹1500 Bonus",
-redeem:"Min ₹100",
-image:"images/yonorummy.png",
-link:"#"
-},
-{
-name:"Spin Winner",
-bonus:"₹1500 Bonus",
-redeem:"Min ₹100",
-image:"images/spinwinner.png",
-link:"#"
-},
-{
-name:"789 Jackpots",
-bonus:"₹1100 Bonus",
-redeem:"Min ₹100",
-image:"images/789jackpots.png",
-link:"#"
-},
-{
-name:"567 Slots",
-bonus:"₹600 Bonus",
-redeem:"Min ₹100",
-image:"images/567slots.png",
-link:"#"
-},
-{
-name:"IND Slots",
-bonus:"₹1200 Bonus",
-redeem:"Min ₹100",
-image:"images/indslots.png",
-link:"#"
-},
-{
-name:"Bingo 101",
-bonus:"₹900 Bonus",
-redeem:"Min ₹100",
-image:"images/bingo101.png",
-link:"#"
-},
-{
-name:"MKM Bet",
-bonus:"₹600 Bonus",
-redeem:"Min ₹100",
-image:"images/mkmbet.png",
-link:"#"
-},
-{
-name:"Royal Slots",
-bonus:"₹100 Bonus",
-redeem:"Min ₹100",
-image:"images/royalslots.png",
-link:"#"
-},
-{
-name:"Golden Win",
-bonus:"₹80 Bonus",
-redeem:"Min ₹100",
-image:"images/goldenwin.png",
-link:"#"
-},
-{
-name:"ABC Rummy",
-bonus:"₹100 Bonus",
-redeem:"Min ₹100",
-image:"images/abcrummy.png",
-link:"#"
-},
-{
-name:"Rummy Gold",
-bonus:"₹100 Bonus",
-redeem:"Min ₹100",
-image:"images/rummygold.png",
-link:"#"
-},
-{
-name:"Rummy Nabob",
-bonus:"₹75 Bonus",
-redeem:"Min ₹100",
-image:"images/rummynabob.png",
-link:"#"
-},
-{
-name:"Moon Club",
-bonus:"₹88 Bonus",
-redeem:"Min ₹100",
-image:"images/moonclub.png",
-link:"#"
-},
-{
-name:"Dream Club",
-bonus:"₹88 Bonus",
-redeem:"Min ₹100",
-image:"images/dreamclub.png",
-link:"#"
-},
-{
-name:"Cash Mania",
-bonus:"₹88 Bonus",
-redeem:"Min ₹100",
-image:"images/cashmania.png",
-link:"#"
-},
-{
-name:"Dragon Win",
-bonus:"₹88 Bonus",
-redeem:"Min ₹100",
-image:"images/dragonwin.png",
-link:"#"
-},
-{
-name:"Tiger Club",
-bonus:"₹88 Bonus",
-redeem:"Min ₹100",
-image:"images/tigerclub.png",
-link:"#"
-},
-{
-name:"Spin Master",
-bonus:"₹88 Bonus",
-redeem:"Min ₹100",
-image:"images/spinmaster.png",
-link:"#"
-},
-{
-name:"Diamond Win",
-bonus:"₹88 Bonus",
-redeem:"Min ₹100",
-image:"images/diamondwin.png",
-link:"#"
-},
-{
-name:"Fortune Club",
-bonus:"₹88 Bonus",
-redeem:"Min ₹100",
-image:"images/fortuneclub.png",
-link:"#"
-},
-{
-name:"Prime Win",
-bonus:"₹88 Bonus",
-redeem:"Min ₹100",
-image:"images/primewin.png",
-link:"#"
-},
-{
-name:"Galaxy Game",
-bonus:"₹120 Bonus",
-redeem:"Min ₹100",
-image:"images/galaxygame.png",
-link:"#"
-},
-{
-name:"Mega Win",
-bonus:"₹150 Bonus",
-redeem:"Min ₹100",
-image:"images/megawin.png",
-link:"#"
+  name:"Club INR VIP",
+  bonus:"₹100 Bonus",
+  redeem:"Min ₹100",
+  image:"images/clubinrvip.png",
+  link:"https://clubinrvip.cc/?code=9VCJY3HUXKW&t=1782988341"
 }
+
 ];
 
 const container = document.getElementById("games");
@@ -366,20 +57,3 @@ games.forEach(game => {
     </div>
   `;
 });
-{
-  name: "Mega Win",
-  bonus: "₹150 Bonus",
-  redeem: "Min ₹100",
-  image: "images/megawin.png",
-  link: "#"
-},
-
-{
-  name: "ABC RUMMY",
-  bonus: "₹100 Bonus",
-  redeem: "Min ₹100",
-  image: "images/abcrummy.png",
-  link: "https://www.11abcrummy.com/?code=75CAZX4PBFD&t=1782980303"
-}
-
-];
